f194bb8a1767987b75a8c99dd792811c0220eb72
daneelshof@gmail.com <daneelshof@gmail.com@e9cc6749-363d-4b58-5071-845f21bc09f4>
2011-04-17 23:33:58 +0000

    Added some class declarations

---
+++ src/Bioinformatics/Sequence.cpp
@@ -0,0 +1,72 @@
+/**
+ *  @file   Sequence.cpp
+ *  @brief  A file for representing genotype sequences
+ *  @author Tony Do,
+ *          Dane Elshof,
+ *          Jonathan Nicholson,
+ *          Dan Thode
+ *  @date   17. April 2011
+ */
+
+ /**
+  *  Default constructor
+  *
+  */
+Sequence::Sequence()
+{
+
+}
+
+~Sequence::Sequence()
+{
+
+}
+
+/**
+ *  Gets the name of a sequence
+ *  @see Sequence();
+ *  @return The string representation of the sequence name
+ */
+std::string Sequence::GetName()
+{
+
+}
+/**
+ *  Sets the name of a sequence
+ *  @see Sequence();
+ *  @param Name The name of the sequence
+ */
+void Sequence::SetName(std::string Name)
+{
+
+}
+
+/**
+ *  Gets the unformatted string representation of the sequence's genotype
+ *  @see Sequence();
+ *  @return The unformatted string representation
+ */
+std::string Sequence::GetSequence()
+{
+
+}
+/**
+ *  Sets the unformatted string representation of the sequence's genotype
+ *  @see Sequence();
+ *  @param Sequence The unformatted string representation of the sequence's genotype
+ */
+void Sequence::SetSequence(std::string Sequence)
+{
+
+}
+
+/**
+ *  Gets the formatted string representation of the sequence's genotype
+ *  @see Sequence();
+ *  @return The formatted string representation of the sequence's genotype
+ */
+std::string Sequence::GetFormattedSequence()
+{
+
+}
+
