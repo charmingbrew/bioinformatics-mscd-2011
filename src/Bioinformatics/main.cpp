5a60adf196718ab6c109b844c9fb65f4730ed662
daneelshof@gmail.com <daneelshof@gmail.com@e9cc6749-363d-4b58-5071-845f21bc09f4>
2011-04-17 21:55:13 +0000

    Doxygen setup

---
+++ src/Bioinformatics/main.cpp
@@ -0,0 +1,20 @@
+/**
+ *  @file   main.cpp
+ *  @brief  Bioinformatics main runner
+ *  @author Tony Do
+ *          Dane Elshof
+ *          Jonathan Nicholson
+ *          Dan Thode
+ *  @date   17. April 2011
+ *
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
