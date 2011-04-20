0bf0a996d714b8dcc0fbe37ba9b89396af755ba7
jnnyboy570 <jnnyboy570@e9cc6749-363d-4b58-5071-845f21bc09f4>
2011-04-20 08:57:35 +0000

    Adding Alignment class and some functionality to Parser. Adding some test files in seq-files for alignment.

---
+++ src/Alignment.h
@@ -0,0 +1,13 @@
+#ifndef _ALIGNMENT_H
+#define _ALIGNMENT_H
+
+#include <string>
+using namespace std;
+
+class Alignment
+{
+  public:
+    int Match(string A, string B);
+};
+
+#endif /* _ALIGNMENT_H */
--- src/Sequence.cpp
+++ src/Sequence.cpp
@@ -6,37 +6,63 @@
 #include <iostream>
 #include <iomanip>
 #include <fstream>
-
 #include "Sequence.h"
+#include "Parser.h"
+#include "Alignment.h"
 
 using std::string;
 using std::cout;
 
-void Sequence::SetSequence(string genome) {
-	genotype = genome;
+/**
+ * Default Constructor
+ */
+Sequence::Sequence()
+{
+
+}
+
+/**
+ *  Constructor that allows both fields to be initialized.
+ *  @param Name The name of the Genotype Sequence
+ *  @param Genome The Sequence of the genotype itself
+ */
+Sequence::Sequence(string Name, string Genome)
+{
+    SetName(Name);
+    SetSequence(Genome);
+}
+
+void Sequence::SetSequence(string Genome)
+{
+    genotype = Genome;
 }
 
 string Sequence::GetSequence() {
 	return genotype;
 }
 
+void Sequence::SetName(string Name)
+{
+    name = Name;
+}
+
+string Sequence::GetName()
+{
+    return name;
+}
+
+
+/**
+ *  @brief Testing Method
+ *  Tests that the Parser and Sequence
+ *  classes work together well.
+ *  TODO: Allow to parse through multiple Sequences
+ */
 int main () {
-	Sequence *s = new Sequence();
 	char hello [101];
 	cout << "Please enter the filepath: ";
 	std::cin.getline(hello, 101);
-	string name;
-	string temp;
-	string genome;
-	std::ifstream file (hello);
-	if (file.is_open()) {
-		if(file.good()) getline(file, name);
-		while (file.good()) {
-			getline(file, temp);
-			genome += temp;
-		}
-	}
-	s->SetSequence(genome);
-	cout << s->GetSequence();
+	Parser * P = new Parser();
+	P->ParseNPrint(hello);
 	return 0;
 }
