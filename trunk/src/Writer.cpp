8d594399e099bed2c709f10524cfd6e98f4c41ff
tony.t.do@gmail.com <tony.t.do@gmail.com@e9cc6749-363d-4b58-5071-845f21bc09f4>
2011-04-20 23:34:41 +0000

    adding more stuff in the writer class

--- trunk/src/Writer.cpp
+++ trunk/src/Writer.cpp
@@ -9,80 +9,15 @@
  */
 
 #include "Writer.h"
+#include "Alignment.h"
+#include "Sequence.h"
 
-/**
- *  Default Constructor
- *
- */
-Writer::Writer()
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main ()
 {
-
-}
-
-~Writer::Writer()
-{
-
-}
-
-/**
- *  a private static method that takes in a pointer to the alignment object
- *  and writes the alignment to a file.
- *  @param File A pointer to the deisired file name
- *  @param alignment A pointer to the aligned genotypes
- */
-static void Writer::AlignWrite(std::string *File, Alignment *alignment)
-{
-
-}
-
-/**
- *  a private static method that takes in a pointer to the sequence object
- *  and writes the sequence to a file.
- *  @param File A pointer to the desired file name
- *  @param sequence A pointer to the sequence of the genotypes
- */
-static void Writer::SeqWrite(std::string *File, Sequence *sequence)
-{
-
-}
-
-/**
- *  a private static method that takes in a pointer to the result object
- *  and writes the result to a file.
- *  @param File A pointer to the desired file name
- *  @param result A pointer to the result from the aligned genotypes
- */
-static void Writer::AlignWrite(std::string *File, Result *result)
-{
-
-}
-
-/**
- *  a public static method that calls the private method AlignWrite
- *  @param Path
- *  @param alignment A pointer to the aligned genotypes
- */
-static void Writer::Write(std::string Path, Alignment *alignment)
-{
-
-}
-
-/**
- *  a public static method that calls the private method SeqWrite
- *  @param Path
- *  @param sequence A pointer to the sequence of the genotypes
- */
-static void Writer::Write(std::string Path, Sequence *sequence)
-{
-
-}
-
-/**
- *  a public static method that calls the private method AlignWrite that writes the results
- *  @param Path
- *  @param result A pointer to the result of the aligned genotypes
- */
-static void Writer::Write(std::string Path, Result *result)
-{
-
+    return 0;
 }
