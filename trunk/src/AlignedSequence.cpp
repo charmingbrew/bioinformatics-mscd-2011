f36f075ec9ca7cae2b62929791c9913b4e9cf6da
millardfillmoreesquire <millardfillmoreesquire@e9cc6749-363d-4b58-5071-845f21bc09f4>
2011-04-21 18:43:36 +0000

    aligned sequence added

--- trunk/src/AlignedSequence.cpp
+++ trunk/src/AlignedSequence.cpp
@@ -1,1 +1,11 @@
 #include "AlignedSequence.h"
+
+void AlignedSequence::SetAlignedGenotype(string gene_in)
+{
+    alignedgenotype = gene_in;
+}
+
+string AlignedSequence::GetAlignedGenotype()
+{
+    return alignedgenotype;
+}
--- trunk/src/AlignedSequence.h
+++ trunk/src/AlignedSequence.h
@@ -1,5 +1,15 @@
 #ifndef _ALIGNEDSEQEUNCE_H
 #define _ALIGNEDSEQUENCE_H
 
+#include <string>
+
+class AlignedSequence : public Sequence {
+    private:
+        string alignedgenotype;
+        int score;
+    public:
+        void SetAlignedGenotype(string gene_in);
+        string GetAlignedGenotype();
+};
 
 #endif
