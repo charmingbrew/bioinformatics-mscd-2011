82e1c849ac3441eafeb1b2c7f9f8ed27cd3e2d2e
millardfillmoreesquire <millardfillmoreesquire@e9cc6749-363d-4b58-5071-845f21bc09f4>
2011-05-17 05:55:14 +0000

    Small documentation update

--- trunk/src/MSAScores.h
+++ trunk/src/MSAScores.h
@@ -6,12 +6,29 @@ class MSAScores
     private:
         double d_score, q_score;
     public:
+        /**
+         * Default constructor
+         */
         MSAScores();
 
+        /**
+         * Returns the Dscore
+         */
         double GetDscore();
+        /**
+         * Returns the Qscore
+         */
         double GetQscore();
 
+        /**
+         * Sets the Dscore
+         * @param num_in The number to set the d score to
+         */
         void SetDscore(double num_in);
+        /**
+         * Sets the Qscore
+         * @param num_in The number to set the q score to
+         */
         void SetQscore(double num_in);
 };
 
